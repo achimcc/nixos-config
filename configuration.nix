34a3ae6ebbba9ef3865716624b1b8a5a005f580b
NixOS User <user@posteo.de>
2026-01-19 17:56:23 +0100

    Upgrade nix zu 25.05

--- configuration.nix
+++ configuration.nix
@@ -240,7 +240,7 @@
   #==========================================
 
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Hack" ]; })
+    nerd-fonts.hack
   ];
 
   # ==========================================
