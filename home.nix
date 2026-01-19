00f019b99838eeb6480af572ef60bb8031242f2a
NixOS User <user@posteo.de>
2026-01-19 17:16:29 +0100

    aktiviere gpg signierung

--- home.nix
+++ home.nix
@@ -69,6 +69,10 @@
     enable = true;
     userName = "NixOS User";
     userEmail = "user@posteo.de";
+    signing = {
+      key = null;  # GPG wählt automatisch den Key passend zur E-Mail
+      signByDefault = true;
+    };
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
