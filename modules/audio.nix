6388618a58cdb93077ba2576f4f066c2536f0437
NixOS User <user@posteo.de>
2026-01-30 08:33:03 +0100

    deklarative bluetooth audio config

--- modules/audio.nix
+++ modules/audio.nix
@@ -13,5 +13,35 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true; # PulseAudio-Kompatibilität
+
+    # WirePlumber Bluetooth-Konfiguration
+    wireplumber.extraConfig."bluetooth" = {
+      "monitor.bluez.properties" = {
+        "bluez5.enable-sbc-xq" = true;
+        "bluez5.enable-msbc" = true;
+        "bluez5.enable-hw-volume" = true;
+        "bluez5.codecs" = [ "sbc" "sbc_xq" "aac" "ldac" "aptx" "aptx_hd" ];
+      };
+      "monitor.bluez.rules" = [
+        {
+          matches = [
+            { "device.name" = "~bluez_card.*"; }
+          ];
+          actions.update-props = {
+            "bluez5.auto-connect" = [ "a2dp_sink" "a2dp_source" "hfp_hf" "hsp_hs" ];
+            "bluez5.hw-volume" = [ "a2dp_sink" "hfp_hf" "hsp_hs" ];
+          };
+        }
+        {
+          matches = [
+            { "node.name" = "~bluez_output.*"; }
+            { "node.name" = "~bluez_input.*"; }
+          ];
+          actions.update-props = {
+            "session.suspend-timeout-seconds" = 0;
+          };
+        }
+      ];
+    };
   };
 }
